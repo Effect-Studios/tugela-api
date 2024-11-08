c8e3749545b943771e2472bbd637051610a1f37c
kryzbone <kryzbone@gmail.com>
2024-11-08 08:23:46 +0000

    wip

--- config/settings/base.py
+++ config/settings/base.py
@@ -67,6 +67,7 @@ LOCAL_APPS = [
     "apps.jobs.apps.JobsConfig",
     "apps.extras.apps.ExtrasConfig",
     "apps.notifications.apps.NotificationsConfig",
+    "apps.recommendations.apps.RecommendationsConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
